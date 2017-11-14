8cd8895ebdf8ce98252d0ba36006b2c0cc8b4c87
Hugo Zamarripa <hzamarripa@hawk.iit.edu>
2017-11-14 02:56:09 -0600

    changed myBtn to doorbell and press d rings doorbell

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,7 +2,15 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doors-closing.mp3');
   doorbell.play();
 }
+function PressD(e) {
+  var keyCode = e.keyCode;
+  if(keyCode === 68){
+    ringDoorbell();
+  }
+}
 $(document).ready(function() {
   $(".js").css('display', 'inline');
 });
-document.getElementById('myBtn').addEventListener("click", ringDoorbell);
+//addEventListene
+document.getElementById('doorbell').addEventListener("click", ringDoorbell);
+window.addEventListener("keydown", PressD);
